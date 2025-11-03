e8dcb5c554cda5b306c718f80e772d20b7dd755a
damianmroz123456-pixel <damianmroz123456@gmail.com>
2025-11-03 08:44:37 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Podaj symbol: ");
+    char s = input.next().charAt(0);
+    System.out.print("Podaj n: ");
+    int n = input.nextInt();
+    for (int i = 1; i <= n; i++){
+      for (int j = 0; j < i; j++){
+        System.out.print(s + " ");
+      }
+      System.out.println();
+    }
+    input.close();
+  }
+}
